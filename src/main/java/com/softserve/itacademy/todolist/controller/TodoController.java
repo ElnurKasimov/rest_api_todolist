b8508b35ec58f958354b7ff5b1948a47e52c7eb8
Elnur Kasimov <ekasimov2012@gmail.com>
2023-09-06 14:52:13 +0300

    cleaned unusable code in TodoController

--- src/main/java/com/softserve/itacademy/todolist/controller/TodoController.java
+++ src/main/java/com/softserve/itacademy/todolist/controller/TodoController.java
@@ -1,21 +1,16 @@
 package com.softserve.itacademy.todolist.controller;
-
 import com.softserve.itacademy.todolist.dto.*;
 import com.softserve.itacademy.todolist.model.ToDo;
-import com.softserve.itacademy.todolist.model.User;
-import com.softserve.itacademy.todolist.service.RoleService;
 import com.softserve.itacademy.todolist.service.ToDoService;
-import com.softserve.itacademy.todolist.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Slf4j
 @RequiredArgsConstructor
